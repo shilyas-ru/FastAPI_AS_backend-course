0d28d52e7a8daa2c85854208d42cb79f82788692
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-03 00:36:55 +0300

    add hashed_password generation, move repositories/ folder to src folder

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,17 +1,22 @@
 from fastapi import APIRouter
 
-from repositories.users import UsersRepository
+from passlib.context import CryptContext
+
+from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd,
 ):
-    hashed_password = "1238129sadj12asd"
+    hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
