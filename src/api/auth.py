aa91df78cb6977cbc5de96da038655e65df57a11
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-04 23:25:49 +0300

    return current user in /only_auth route

--- src/api/auth.py
+++ src/api/auth.py
@@ -42,5 +42,9 @@ async def login_user(
 async def only_auth(
         request: Request,
 ):
-    ...
-    access_token = "..." or None
+    access_token = request.cookies.get("access_token", None)
+    data = AuthService().encode_token(access_token)
+    user_id = data["user_id"]
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+        return user
