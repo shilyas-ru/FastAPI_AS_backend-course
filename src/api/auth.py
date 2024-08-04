e2ddd9689c34b749ab0bca82973f249a3cb6cab7
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-04 23:25:06 +0300

    create /only_auth route for task

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
+from starlette.requests import Request
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -35,3 +36,11 @@ async def login_user(
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+
+@router.get("/only_auth")
+async def only_auth(
+        request: Request,
+):
+    ...
+    access_token = "..." or None
