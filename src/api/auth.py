9c520ef8ffb80879c03ad8b1618fef7192f796fc
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-04 22:38:04 +0300

    fix previous commit: use get_user_with_hashed_password

--- src/api/auth.py
+++ src/api/auth.py
@@ -27,7 +27,7 @@ async def login_user(
         response: Response,
 ):
     async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
         if not AuthService().verify_password(data.password, user.hashed_password):
