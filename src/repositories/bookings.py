5d0fb0edb22b343ac35292de6da8336e3cc3982e
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-08 23:38:34 +0300

    add celery beat, add beat schedule and task

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,9 +1,20 @@
+from datetime import date
+
+from sqlalchemy import select
+
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.mappers import BookingDataMapper
-from src.schemas.bookings import Booking
 
 
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
     mapper = BookingDataMapper
+
+    async def get_bookings_with_today_checkin(self):
+        query = (
+            select(BookingsOrm)
+            .filter(BookingsOrm.date_from == date.today())
+        )
+        res = await self.session.execute(query)
+        return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
