99db10c98cf51b1c112ddac76ba423a057b5eab7
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-08 23:51:25 +0300

    remove example with fastapi infinite async task

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -15,7 +14,6 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 
-from src.api.dependencies import get_db
 from src.init import redis_manager
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
@@ -25,22 +23,9 @@ from src.api.facilities import router as router_facilities
 from src.api.images import router as router_images
 
 
-async def send_emails_bookings_today_checkin():
-    async for db in get_db():
-        bookings = await db.bookings.get_bookings_with_today_checkin()
-        print(f"{bookings=}")
-
-
-async def run_send_email_regularly():
-    while True:
-        await send_emails_bookings_today_checkin()
-        await asyncio.sleep(5)
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # При старте приложения
-    asyncio.create_task(run_send_email_regularly())
     await redis_manager.connect()
     FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")
     yield
