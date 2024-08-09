8527938585d8d0dd860ad5a12006936ea3ee6811
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-09 16:44:23 +0300

    use httpx to register user, use async_session_maker_null_pool instead of async_session_maker

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 
 from src.config import settings
 from src.database import Base, engine_null_pool
+from src.main import app
 from src.models import *
+from httpx import AsyncClient
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -11,7 +13,19 @@ def check_test_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_test_mode):
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "kot@pes.com",
+                "password": "1234"
+            }
+        )
