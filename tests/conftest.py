d547a8964e7293d7ce71993af0a20b75fde7fb9b
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-09 19:46:26 +0300

    create authenticated_ac

--- tests/conftest.py
+++ tests/conftest.py
@@ -71,3 +71,16 @@ async def register_user(ac, setup_database):
             "password": "1234"
         }
     )
+
+
+@pytest.fixture(scope="session")
+async def authenticated_ac(register_user, ac):
+    await ac.post(
+        "/auth/login",
+        json={
+            "email": "kot@pes.com",
+            "password": "1234"
+        }
+    )
+    assert ac.cookies["access_token"]
+    yield ac
