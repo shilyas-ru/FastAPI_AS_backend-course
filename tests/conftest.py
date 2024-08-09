6edeebf2ee3ef0493e8125685cb95500c9607de3
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-09 19:14:31 +0300

    use dependency_overrides, add hotels GET api test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import json
 
 import pytest
 
+from src.api.dependencies import get_db
 from src.config import settings
 from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.main import app
@@ -18,10 +19,18 @@ def check_test_mode():
     assert settings.MODE == "TEST"
 
 
-@pytest.fixture(scope="function")
-async def db() -> DBManager:
+async def get_db_null_pool():
     async with DBManager(session_factory=async_session_maker_null_pool) as db:
         yield db
+
+
+@pytest.fixture(scope="function")
+async def db() -> DBManager:
+    async for db in get_db_null_pool():
+        yield db
+
+
+app.dependency_overrides[get_db] = get_db_null_pool
 
 
 @pytest.fixture(scope="session", autouse=True)
