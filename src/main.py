45a89ecdc16f27490b30a8293413288a593953f6
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-11 23:27:41 +0300

    add logging

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
-from fastapi.openapi.docs import get_swagger_ui_html
-import uvicorn
-
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-
+import logging
 import sys
 from pathlib import Path
 
+from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+import uvicorn
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+logging.basicConfig(level=logging.DEBUG)
 
 from src.init import redis_manager
 from src.api.hotels import router as router_hotels
@@ -28,6 +27,7 @@ async def lifespan(app: FastAPI):
     # При старте приложения
     await redis_manager.connect()
     FastAPICache.init(RedisBackend(redis_manager._redis), prefix="fastapi-cache")
+    logging.info("FastAPI cache initialized")
     yield
     await redis_manager.close()
     # При выключении/перезагрузке приложения
