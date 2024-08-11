c3ab8afa8e07910097aae945f6d2f082a313021d
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-12 00:31:29 +0300

    create services for rooms, facilities and images

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,9 +1,10 @@
 from datetime import date
 
 from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import selectinload
 
-from src.database import Base
+from src.exceptions import RoomNotFoundException
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
 from src.repositories.mappers.mappers import RoomDataMapper, RoomDataWithRelsMapper
@@ -33,12 +34,13 @@ class RoomsRepository(BaseRepository):
             for model in result.unique().scalars().all()
         ]
 
-    async def get_one_or_none_with_rels(self, **filter_by):
+    async def get_one_with_rels(self, **filter_by):
         query = (
             select(self.model).options(selectinload(self.model.facilities)).filter_by(**filter_by)  # type: ignore
         )
         result = await self.session.execute(query)
-        model = result.scalars().one_or_none()
-        if model is None:
-            return None
+        try:
+            model = result.scalar_one()
+        except NoResultFound:
+            raise RoomNotFoundException
         return RoomDataWithRelsMapper.map_to_domain_entity(model)
