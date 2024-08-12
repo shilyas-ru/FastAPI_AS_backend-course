969a5e823ce1cbee650b81ea3cfb70cb3af23dfb
Artem Shumeiko <shumeykoartem@mail.ru>
2024-08-12 16:29:51 +0300

    add Dockerfile, .dockerignore, change host to 0.0.0.0

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ import uvicorn
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 from src.init import redis_manager
 from src.api.hotels import router as router_hotels
@@ -55,4 +55,4 @@ async def custom_swagger_ui_html():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
