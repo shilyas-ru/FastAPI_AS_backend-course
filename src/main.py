7ec3e1c1161bd1e2b21414d9d27a6ab9f984f17d
Artem Shumeiko <shumeykoartem@mail.ru>
2024-09-08 18:29:59 +0300

    chore: add CORS and index.html

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
@@ -52,6 +53,8 @@ async def custom_swagger_ui_html():
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
     )
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"])
 
 
 if __name__ == "__main__":
